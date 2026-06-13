13f0cd187ef7b697116e7c0ad60d5ea99e78855f
Gustavo Augusto Leite <guxtagx@gmail.com>
2026-06-13 10:58:35 -0300

    Implementa calculadora de expressões pós-fixas
    Adiciona funções para conversão de expressões pós-fixas para infixas e cálculo do valor da expressão utilizando pilhas.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "calculadora.h"
 
 int main() {
@@ -6,19 +7,22 @@ int main() {
     char expr[512];
 
     printf("Digite a expressao pos-fixa:\n");
-    fgets(expr, sizeof(expr), stdin);
+
+    if (fgets(expr, sizeof(expr), stdin) == NULL) {
+        printf("Erro na leitura.\n");
+        return 1;
+    }
 
     expr[strcspn(expr, "\n")] = '\0';
 
     char *infixa = getInFixa(expr);
 
-    if(infixa == NULL) {
+    if (infixa == NULL) {
         printf("Expressao invalida!\n");
         return 1;
     }
 
     printf("\nForma infixa:\n%s\n", infixa);
-
     printf("\nResultado:\n%.4f\n", getValor(expr));
 
     return 0;
